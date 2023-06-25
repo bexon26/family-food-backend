757759173d2097ad0c39865ec9f5ce6728fb65a6
Your Name <you@example.com>
2023-06-25 13:29:21 +0300

    comleate realway

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 import mongoose from "mongoose";
 
-import bodyParser from "body-parser"
+
 
 
 import {
@@ -28,11 +28,7 @@ mongoose
 
   
   const app = express();
-  // app.use(bodyParser.urlencoded({ extended: false }));
-  // app.use(bodyParser.json());
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
@@ -45,6 +41,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
